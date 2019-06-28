e1d09b32959fdeef9c27c371c0c2b36e7a3b6485
bellycat77 <bellycat77@gmail.com>
2019-06-29 02:07:38 +0700

    Add product page tests

--- pages/locators.py
+++ pages/locators.py
@@ -1,11 +1,22 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators(object):
+class BasePageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 
 class LoginPageLocators(object):
     REGISTRATION_FORM = (By.ID, "register_form")
     LOGIN_FORM = (By.ID, "login_form")
 
+
+class ProductPageLocators(object):
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    INFO_MESSAGE_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages div:nth-child(1) strong")
+    INFO_MESSAGE_CART_PRICE = (By.CSS_SELECTOR, "#messages div:nth-child(3) strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "div.product_main .price_color")
+
+#class CartPageLocators(object):
+#    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
---
+++ test_product_page.py
@@ -0,0 +1,29 @@
+import pytest
+from pages.product_page import ProductPage
+
+
+LINK = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
+
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_cart(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_cart()
+    page.check_for_message_product_is_added()
+    page.check_for_correct_cart_price()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, LINK)
+    page.open()
+    page.check_adding_message_not_present()
